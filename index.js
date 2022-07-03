ecd9bc872e7353a865091a3bcbd38adda7b55d9f
Lukas Mills <lukascodes64@gmail.com>
2022-07-03 19:02:44 -0400

    commit tree 046e34b47a8d2e616c28ad982fc01762fec5a908

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+let drivers = [`Antonia`, `Nuru`, `Amari`, `Mo`]
+
+const returnFirstTwoDrivers = function(){
+    return drivers.slice(0,2)
+}
+const returnLastTwoDrivers = function(){
+    return drivers.slice (-2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers(), returnFirstTwoDrivers()]
